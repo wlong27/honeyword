d920f0502bd9fb330b88627494efa4d3256641ba
wlong <wlong27@outlook.sg>
2019-04-06 16:49:43 +0800

    Added base files

---
+++ app.py
@@ -0,0 +1,60 @@
+# import the Flask class from the flask module
+from flask import Flask, render_template, redirect, url_for, request, session, flash, g
+from functools import wraps
+import sqlite3
+
+# create the application object
+app = Flask(__name__)
+
+# config
+app.secret_key = 'my precious'
+app.database = "sample.db"
+
+
+# login required decorator
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('You need to login first.')
+            return redirect(url_for('login'))
+    return wrap
+
+# use decorators to link the function to a url
+@app.route('/')
+@login_required
+def home():
+    g.db = connect_db()
+    cur = g.db.execute('select * from Users')
+    users = [dict(username=row[0], hash=row[1]) for row in cur.fetchall()]
+    g.db.close()
+    return render_template('index.html',users=users)
+
+# route for handling the login page logic
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in.')
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+@login_required
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out.')
+    return redirect(url_for('login'))
+
+def connect_db():
+    return sqlite3.connect(app.database)
+
+# start the server with the 'run()' method
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ hello.py
@@ -0,0 +1,12 @@
+msg = "Hello World"
+print(msg)
+
+import http.server
+import socketserver
+
+PORT = 8080
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
