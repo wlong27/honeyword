c071367a1dd7ca9240880cb773504b93f6e4cb76
wlong <wlong27@outlook.sg>
2019-04-09 20:08:29 +0800

    removed unnecessary files

--- sql.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-
-with sqlite3.connect("sample.db") as connection:
-    c = connection.cursor()
-    c.execute('CREATE TABLE Users(userName TEXT, passwdHash TEXT)')
-    c.execute('INSERT INTO Users VALUES("Good", "test")')
-    c.execute('INSERT INTO Users VALUES("Good2", "test2")')
